76537cdff6f89c4881578f25ca85066665c1edcf
Helen Kruminia <lena_kru@tut.by>
2022-08-09 13:42:40 +0300

    feat: working solution

--- src/index.js
+++ src/index.js
@@ -41,23 +41,26 @@ function decode(expr) {
     // write your solution here
     let decString = '';
     let i = 0;
+    let decSymbol;
     while (i < expr.length) {
-        let decSymbol ='';
+        decSymbol ='';
             for (let j = 0; j < 10; j++) {
                 if (expr[i] == '0') i++;
                 else if (expr[i] == '1') {
                     if (expr[i+1] == '0') decSymbol += '.';
                     else if (expr[i+1] == '1') decSymbol += '-';
-                    i+=2;
+                    i += 2;
+                    j++;
                 }
                 else if (expr[i] == '*') {
                     decString += ' ';
-                    i += 9;
+                    i += 10;
+                    j += 10;
                 }
             }
-            if (decSymbol !== '') decString += MORSE_TABLE.[decSymbol];
+            if (decSymbol !== '') decString += MORSE_TABLE[decSymbol];
     }
-
+return (decString);
 }
 
 module.exports = {
