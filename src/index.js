de33f05341b9f5cbaab55ba65ee6469ebd74584e
Helen Kruminia <lena_kru@tut.by>
2022-08-09 13:20:19 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -39,6 +39,25 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let decString = '';
+    let i = 0;
+    while (i < expr.length) {
+        let decSymbol ='';
+            for (let j = 0; j < 10; j++) {
+                if (expr[i] == '0') i++;
+                else if (expr[i] == '1') {
+                    if (expr[i+1] == '0') decSymbol += '.';
+                    else if (expr[i+1] == '1') decSymbol += '-';
+                    i+=2;
+                }
+                else if (expr[i] == '*') {
+                    decString += ' ';
+                    i += 9;
+                }
+            }
+            if (decSymbol !== '') decString += MORSE_TABLE.[decSymbol];
+    }
+
 }
 
 module.exports = {
